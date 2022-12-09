9e4de19e73ca74f1a61b2e2301f5f5ee216155ac
Softyn Co <softynco.ltd@Softyns-Mac-mini.local>
2022-12-09 15:37:30 +0700

    Updates

---
+++ precache-manifest.42e7751cf6a3285fda376d816612fafc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fad65d3428420ab6b6ea",
+    "url": "/react_push_noti/static/css/main.9626b455.chunk.css"
+  },
+  {
+    "revision": "fad65d3428420ab6b6ea",
+    "url": "/react_push_noti/static/js/main.fad65d34.chunk.js"
+  },
+  {
+    "revision": "af0aac77d8aecc35095c",
+    "url": "/react_push_noti/static/js/1.af0aac77.chunk.js"
+  },
+  {
+    "revision": "d70a34978c36ee23d4bb",
+    "url": "/react_push_noti/static/js/2.d70a3497.chunk.js"
+  },
+  {
+    "revision": "f27e776aa672e13aec76",
+    "url": "/react_push_noti/static/js/runtime~main.f27e776a.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react_push_noti/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "f17067d49ae8139e1349d8ff52371315",
+    "url": "/react_push_noti/index.html"
+  }
+];
